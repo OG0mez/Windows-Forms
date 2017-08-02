3cfae69845cd710ac0c4dd044cbd0d3eb1252d93
hjohnson12 <hlj512@hotmail.com>
2017-08-02 11:28:38 -0400

    Add files via upload

---
+++ CSharp/MouseBrush/MouseBrush/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace MouseBrush
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAquaMarine = new System.Windows.Forms.Button();
+            this.btnPeru = new System.Windows.Forms.Button();
+            this.btnTeal = new System.Windows.Forms.Button();
+            this.btnNavy = new System.Windows.Forms.Button();
+            this.btnLightCoral = new System.Windows.Forms.Button();
+            this.btnKhaki = new System.Windows.Forms.Button();
+            this.btnOrangeRed = new System.Windows.Forms.Button();
+            this.btnMaroon = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.Purple;
+            this.button1.Location = new System.Drawing.Point(20, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 62);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnPurple_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.Blue;
+            this.button2.Location = new System.Drawing.Point(86, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 62);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.BackColor = System.Drawing.Color.Turquoise;
+            this.button3.Location = new System.Drawing.Point(152, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 62);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.btnTurquoise_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.BackColor = System.Drawing.Color.Green;
+            this.button4.Location = new System.Drawing.Point(218, 38);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(60, 62);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.BackColor = System.Drawing.Color.Yellow;
+            this.button5.Location = new System.Drawing.Point(284, 38);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(60, 62);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.btnYellow_Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.BackColor = System.Drawing.Color.Orange;
+            this.button6.Location = new System.Drawing.Point(350, 38);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(60, 62);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.btnOrange_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(456, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Pen Color";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(955, 38);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(132, 59);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Choose Color";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnAquaMarine);
+            this.panel1.Controls.Add(this.btnPeru);
+            this.panel1.Controls.Add(this.btnTeal);
+            this.panel1.Controls.Add(this.btnNavy);
+            this.panel1.Controls.Add(this.btnLightCoral);
+            this.panel1.Controls.Add(this.btnKhaki);
+            this.panel1.Controls.Add(this.btnOrangeRed);
+            this.panel1.Controls.Add(this.btnMaroon);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1105, 113);
+            this.panel1.TabIndex = 8;
+            // 
+            // btnAquaMarine
+            // 
+            this.btnAquaMarine.AutoSize = true;
+            this.btnAquaMarine.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnAquaMarine.Location = new System.Drawing.Point(746, 38);
+            this.btnAquaMarine.Name = "btnAquaMarine";
+            this.btnAquaMarine.Size = new System.Drawing.Size(60, 62);
+            this.btnAquaMarine.TabIndex = 13;
+            this.btnAquaMarine.UseVisualStyleBackColor = false;
+            this.btnAquaMarine.Click += new System.EventHandler(this.btnAquaMarine_Click);
+            // 
+            // btnPeru
+            // 
+            this.btnPeru.BackColor = System.Drawing.Color.Peru;
+            this.btnPeru.Location = new System.Drawing.Point(878, 38);
+            this.btnPeru.Name = "btnPeru";
+            this.btnPeru.Size = new System.Drawing.Size(60, 62);
+            this.btnPeru.TabIndex = 15;
+            this.btnPeru.UseVisualStyleBackColor = false;
+            this.btnPeru.Click += new System.EventHandler(this.btnPeru_Click);
+            // 
+            // btnTeal
+            // 
+            this.btnTeal.AutoSize = true;
+            this.btnTeal.BackColor = System.Drawing.Color.Teal;
+            this.btnTeal.Location = new System.Drawing.Point(680, 38);
+            this.btnTeal.Name = "btnTeal";
+            this.btnTeal.Size = new System.Drawing.Size(60, 62);
+            this.btnTeal.TabIndex = 12;
+            this.btnTeal.UseVisualStyleBackColor = false;
+            this.btnTeal.Click += new System.EventHandler(this.btnTeal_Click);
+            // 
+            // btnNavy
+            // 
+            this.btnNavy.AutoSize = true;
+            this.btnNavy.BackColor = System.Drawing.Color.Navy;
+            this.btnNavy.Location = new System.Drawing.Point(812, 38);
+            this.btnNavy.Name = "btnNavy";
+            this.btnNavy.Size = new System.Drawing.Size(60, 62);
+            this.btnNavy.TabIndex = 14;
+            this.btnNavy.UseVisualStyleBackColor = false;
+            this.btnNavy.Click += new System.EventHandler(this.btnNavy_Click);
+            // 
+            // btnLightCoral
+            // 
+            this.btnLightCoral.AutoSize = true;
+            this.btnLightCoral.BackColor = System.Drawing.Color.LightCoral;
+            this.btnLightCoral.Location = new System.Drawing.Point(482, 38);
+            this.btnLightCoral.Name = "btnLightCoral";
+            this.btnLightCoral.Size = new System.Drawing.Size(60, 62);
+            this.btnLightCoral.TabIndex = 9;
+            this.btnLightCoral.UseVisualStyleBackColor = false;
+            this.btnLightCoral.Click += new System.EventHandler(this.btnLightCoral_Click);
+            // 
+            // btnKhaki
+            // 
+            this.btnKhaki.AutoSize = true;
+            this.btnKhaki.BackColor = System.Drawing.Color.Khaki;
+            this.btnKhaki.Location = new System.Drawing.Point(614, 38);
+            this.btnKhaki.Name = "btnKhaki";
+            this.btnKhaki.Size = new System.Drawing.Size(60, 62);
+            this.btnKhaki.TabIndex = 11;
+            this.btnKhaki.UseVisualStyleBackColor = false;
+            this.btnKhaki.Click += new System.EventHandler(this.btnKhaki_Click);
+            // 
+            // btnOrangeRed
+            // 
+            this.btnOrangeRed.AutoSize = true;
+            this.btnOrangeRed.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnOrangeRed.Location = new System.Drawing.Point(416, 38);
+            this.btnOrangeRed.Name = "btnOrangeRed";
+            this.btnOrangeRed.Size = new System.Drawing.Size(60, 62);
+            this.btnOrangeRed.TabIndex = 8;
+            this.btnOrangeRed.UseVisualStyleBackColor = false;
+            this.btnOrangeRed.Click += new System.EventHandler(this.btnOrangeRed_Click);
+            // 
+            // btnMaroon
+            // 
+            this.btnMaroon.AutoSize = true;
+            this.btnMaroon.BackColor = System.Drawing.Color.Maroon;
+            this.btnMaroon.Location = new System.Drawing.Point(548, 38);
+            this.btnMaroon.Name = "btnMaroon";
+            this.btnMaroon.Size = new System.Drawing.Size(60, 62);
+            this.btnMaroon.TabIndex = 10;
+            this.btnMaroon.UseVisualStyleBackColor = false;
+            this.btnMaroon.Click += new System.EventHandler(this.btnMaroon_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1105, 894);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Mouse Brush";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseMove);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAquaMarine;
+        private System.Windows.Forms.Button btnPeru;
+        private System.Windows.Forms.Button btnTeal;
+        private System.Windows.Forms.Button btnNavy;
+        private System.Windows.Forms.Button btnLightCoral;
+        private System.Windows.Forms.Button btnKhaki;
+        private System.Windows.Forms.Button btnOrangeRed;
+        private System.Windows.Forms.Button btnMaroon;
+    }
+}
+
---
+++ CSharp/MouseBrush/MouseBrush/Form1.cs
@@ -0,0 +1,144 @@
+﻿/*
+ *  Programmer: Hunter Johnson
+ *  Info: Demonstrates graphics object and making a "mouse brush"
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseBrush
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Graphics gfx; // graphics object
+        Random rd = new Random();
+        Pen p = new Pen(Color.Black); // pen object
+        int x, y;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            gfx = this.CreateGraphics(); // create graphics
+            MinimizeBox = false;
+            MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+        private void MainForm_Close(object sender, EventArgs e)
+        {
+            gfx.Dispose(); // dispose of the graphics
+        }
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            Rectangle rectEllipse = new Rectangle();
+            if (e.Button != MouseButtons.Left)
+                return;
+            x = e.X - 1;
+            y = e.Y - 1;
+            rectEllipse.X = e.X - 1;
+            rectEllipse.Y = e.Y - 1;
+            rectEllipse.Width = 7;
+            rectEllipse.Height = 7;
+            gfx.DrawEllipse(p, rectEllipse);
+        }
+
+        private void btnPurple_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Purple;
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Blue;
+        }
+
+        private void btnTurquoise_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Turquoise;
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Green;
+        }
+
+        private void btnYellow_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Yellow;
+        }
+
+        private void btnOrange_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Orange;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Generates Random Color on Right-Click
+            if (e.Button == MouseButtons.Right)
+                gfx.Clear(Color.FromArgb(rd.Next(0, 255), rd.Next(0, 255), rd.Next(0, 255)));
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // set the selected color to the RTB's forecolor
+                p.Color = colorDialog1.Color;
+            }
+        }
+
+        private void btnAquaMarine_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Aquamarine;
+        }
+
+        private void btnPeru_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Peru;
+        }
+
+        private void btnTeal_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Teal;
+        }
+
+        private void btnNavy_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Navy;
+        }
+
+        private void btnLightCoral_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.LightCoral;
+        }
+
+        private void btnKhaki_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Khaki;
+        }
+
+        private void btnOrangeRed_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.OrangeRed;
+        }
+
+        private void btnMaroon_Click(object sender, EventArgs e)
+        {
+            p.Color = Color.Maroon;
+        }
+    }
+}
