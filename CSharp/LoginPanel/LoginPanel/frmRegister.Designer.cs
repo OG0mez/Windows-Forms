5231607e3c47a576333ea9aa6717b1382ac1610d
hjohnson12 <hlj512@hotmail.com>
2017-08-02 10:05:02 -0400

    Add files via upload

---
+++ CSharp/LoginPanel/LoginPanel/frmRegister.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace LoginPanel
+{
+    partial class frmRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRegister));
+            this.myDatabaseData = new LoginPanel.MyDatabaseData();
+            this.tbl_LoginBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbl_LoginTableAdapter = new LoginPanel.MyDatabaseDataTableAdapters.tbl_LoginTableAdapter();
+            this.tableAdapterManager = new LoginPanel.MyDatabaseDataTableAdapters.TableAdapterManager();
+            this.tbl_LoginBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.tbl_LoginBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.tbl_LoginDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.myDatabaseData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbl_LoginBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbl_LoginBindingNavigator)).BeginInit();
+            this.tbl_LoginBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbl_LoginDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myDatabaseData
+            // 
+            this.myDatabaseData.DataSetName = "MyDatabaseData";
+            this.myDatabaseData.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tbl_LoginBindingSource
+            // 
+            this.tbl_LoginBindingSource.DataMember = "tbl_Login";
+            this.tbl_LoginBindingSource.DataSource = this.myDatabaseData;
+            // 
+            // tbl_LoginTableAdapter
+            // 
+            this.tbl_LoginTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.tbl_LoginTableAdapter = this.tbl_LoginTableAdapter;
+            this.tableAdapterManager.UpdateOrder = LoginPanel.MyDatabaseDataTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // tbl_LoginBindingNavigator
+            // 
+            this.tbl_LoginBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.tbl_LoginBindingNavigator.BindingSource = this.tbl_LoginBindingSource;
+            this.tbl_LoginBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.tbl_LoginBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.tbl_LoginBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.tbl_LoginBindingNavigatorSaveItem});
+            this.tbl_LoginBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.tbl_LoginBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.tbl_LoginBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.tbl_LoginBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.tbl_LoginBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.tbl_LoginBindingNavigator.Name = "tbl_LoginBindingNavigator";
+            this.tbl_LoginBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.tbl_LoginBindingNavigator.Size = new System.Drawing.Size(411, 25);
+            this.tbl_LoginBindingNavigator.TabIndex = 0;
+            this.tbl_LoginBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 15);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
+            // tbl_LoginBindingNavigatorSaveItem
+            // 
+            this.tbl_LoginBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbl_LoginBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("tbl_LoginBindingNavigatorSaveItem.Image")));
+            this.tbl_LoginBindingNavigatorSaveItem.Name = "tbl_LoginBindingNavigatorSaveItem";
+            this.tbl_LoginBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.tbl_LoginBindingNavigatorSaveItem.Text = "Save Data";
+            this.tbl_LoginBindingNavigatorSaveItem.Click += new System.EventHandler(this.tbl_LoginBindingNavigatorSaveItem_Click);
+            // 
+            // tbl_LoginDataGridView
+            // 
+            this.tbl_LoginDataGridView.AutoGenerateColumns = false;
+            this.tbl_LoginDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tbl_LoginDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.tbl_LoginDataGridView.DataSource = this.tbl_LoginBindingSource;
+            this.tbl_LoginDataGridView.Location = new System.Drawing.Point(28, 41);
+            this.tbl_LoginDataGridView.Name = "tbl_LoginDataGridView";
+            this.tbl_LoginDataGridView.Size = new System.Drawing.Size(300, 220);
+            this.tbl_LoginDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "UserName";
+            this.dataGridViewTextBoxColumn2.HeaderText = "UserName";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Password";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Password";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // frmRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 325);
+            this.Controls.Add(this.tbl_LoginDataGridView);
+            this.Controls.Add(this.tbl_LoginBindingNavigator);
+            this.Name = "frmRegister";
+            this.Text = "frmRegister";
+            this.Load += new System.EventHandler(this.frmRegister_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.myDatabaseData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbl_LoginBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbl_LoginBindingNavigator)).EndInit();
+            this.tbl_LoginBindingNavigator.ResumeLayout(false);
+            this.tbl_LoginBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbl_LoginDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MyDatabaseData myDatabaseData;
+        private System.Windows.Forms.BindingSource tbl_LoginBindingSource;
+        private MyDatabaseDataTableAdapters.tbl_LoginTableAdapter tbl_LoginTableAdapter;
+        private MyDatabaseDataTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator tbl_LoginBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton tbl_LoginBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView tbl_LoginDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+    }
+}
---
+++ CSharp/LoginPanel/LoginPanel/frmRegister.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginPanel
+{
+    public partial class frmRegister : Form
+    {
+        public frmRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void tbl_LoginBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tbl_LoginBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.myDatabaseData);
+
+        }
+
+        private void frmRegister_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'myDatabaseData.tbl_Login' table. You can move, or remove it, as needed.
+            this.tbl_LoginTableAdapter.Fill(this.myDatabaseData.tbl_Login);
+
+        }
+    }
+}
